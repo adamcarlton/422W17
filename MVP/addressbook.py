6ce0261404f10032ae6efb8b18f58e9c143a8981
zachary-trudo <zachary.a.trudo@intel.com>
2017-01-18 14:07:58 -0800

    Initial work on MVP
    Problem: We don't have an MVP!
    
    Solution: Accidentally made the basis for it.
    
    Filechanges:
    - MVP/address.py
      - Added the address object
    - MVP/addressBook.py
      - Added the address book object
    - MVP/fileops.py
      - added the methods for interacting with files
    - MVP/tests/test_address_book
      - added an initial test address book
    - MVP/tests/FileOpsTest.py
      - Added some basic unit tests for file operations.

---
+++ MVP/addressbook.py
@@ -0,0 +1,51 @@
+from address import Address
+from address import TypeException
+
+class AddressBook(object):
+  """ A basic address book object. """
+  def __init__(self, fileName, addresses = []):
+    """ Basic initialization class.
+
+    Args:
+      fileName (str): name and hopefully path of the file.
+      addresses (list): List of addresses
+    """
+    self.fileName = fileName
+    self.addresses = []
+    for address in addresses:
+      self.add_address(address)
+
+  def to_dict(self):
+    addresses = [x.to_dict() for x in self.addresses]
+    retVal = [ {"address": address} for address in addresses]
+    return retVal
+
+  def add_address(self, address):
+    """ Adds an address to the address book.
+
+    Verifies that the address is actually of the Address object type. Will
+    throw an exception if it isn't.
+
+    Args:
+      address (Address): The Address object we want to append.
+    """
+    if not isinstance(address, Address):
+      raise TypeException("address is not of type Address, got type {}".format(type(address)))
+    self.addresses.append(address)
+
+  def delete_address(self, address):
+    """ Deletes an address from the address book.
+
+    Verifies that the address is actually of the Address object type. Will
+    throw an exception if it isn't. If the address to be deleted is not 
+    actually in the address book - will ignore the delete operation.
+
+    Args:
+      address (Address): the Address object we want to delete.
+    """
+    if not isinstance(address, Address):
+      raise TypeException("address is not of type Address, got type {}".format(type(address)))
+    try:
+        self.addresses.remove(address)
+    except ValueError as e:
+        pass
